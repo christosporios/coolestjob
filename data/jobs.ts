cb15d02e16817d64230f84692ef81eceb5ffb0a5
Theodore Keloglou <zf@sirodoht.com>
2025-05-01 09:11:52 +0100

    Add executive assistant job for pod network

--- data/jobs.ts
+++ data/jobs.ts
@@ -97,4 +97,15 @@ export const jobs: JobType[] = [
     location: "Athens / Remote",
     externalLink: "https://schemalabs.gr/careers/founding-engineer"
   },
+  {
+    id: "executive-assistant-pod",
+    title: "🙋 Executive Assistant and Office Manager",
+    description: "In this role, you will provide comprehensive support to our senior executives as well as ensure the smooth operation of our office.",
+    company: "pod network",
+    companyDescription: "We are an ambitious, early-stage Web3 startup building a revolutionary new layer-1 protocol designed to bring Web2-like experience to Web3 systems.",
+    compensation: 16200,
+    email: "talent@pod.network",
+    location: "Athens, Greece",
+    externalLink: "https://pod-network.notion.site/Executive-Assistant-Office-Manager-168a9700b29b807ea6f9c7908b6c5645?pvs=4"
+  },
 ]
