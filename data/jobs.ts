2b4f7d098633c3dae5dd079c75f1d77c0b85cc8a
Christos Porios <christos.porios@gmail.com>
2025-05-12 18:21:15 +0300

    add quirio

--- data/jobs.ts
+++ data/jobs.ts
@@ -12,4 +12,14 @@ export const jobs: JobType[] = [
     location: "Athens, Greece",
     externalLink: "https://pod-network.notion.site/Executive-Assistant-Office-Manager-168a9700b29b807ea6f9c7908b6c5645?pvs=4"
   },
+  {
+    id: "qurio-founding-eng",
+    title: "🛠️ Founding Engineer",
+    description: "We are looking for a founding engineer to support our clients in the U.S. and E.U.",
+    company: "Qurio - Growing Newsrooms at the Speed of Trust",
+    companyDescription: "Building technology that helps newsrooms stay relevant to their audiences",
+    compensation: 65000,
+    email: "tassos@getqurio.com",
+    location: "Athens, Greece - No Remote"
+  },
 ]
